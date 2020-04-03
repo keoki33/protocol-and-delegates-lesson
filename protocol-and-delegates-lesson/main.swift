826e9f8e3b3f63285fcb47557356a07bdf221045
keoki33 <gwong323@yahoo.com>
2020-04-03 22:37:27 +1100

    lesson done

--- protocol-and-delegates-lesson/main.swift
+++ protocol-and-delegates-lesson/main.swift
@@ -1,3 +1,67 @@
 
 
+protocol AdvancedLifeSupport {
+    func performCpr()
+}
+
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+
+    func assessSituation()  {
+        print("get details")
+    }
+    
+    func medicalEmergency()  {
+        delegate?.performCpr()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    
+    
+    func performCpr() {
+        print("The paramedic will do chest compressions.")
+    }
+    
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCpr() {
+        print("doctor does chest compression")
+    }
+    
+}
+
+
+class Surgeon: Doctor {
+    override  func  performCpr() {
+        super.performCpr()
+        print("sing")
+    }
+}
+
+
+let emilio = EmergencyCallHandler()
+let pete = Paramedic(handler: emilio)
+emilio.medicalEmergency()
+
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
+
+
+
 
